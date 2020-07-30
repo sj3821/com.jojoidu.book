aabfdd75c64c6db3ac5f33b3ac3ddce22a34fba8
sj3821 <sj3821@gmail.com>
2020-07-30 16:27:43 +0900

    dto test

--- src/main/java/com/jojoidu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoidu/book/springboot/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.jojoidu.book.springboot.web;
+import com.jojoidu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -9,6 +11,12 @@ public class HelloController {
     @GetMapping("/hello") // Get요청을 받을 수 있는 API = @RequestMapping(method=RequestMethod.GET)
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, //@RequestParam 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션이다.
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
     }
 
 }
--- src/test/java/com/jojoidu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoidu/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,7 @@
 package com.jojoidu.book.springboot.web;
 
 import com.jojoidu.book.springboot.web.HelloController;
+import jdk.net.SocketFlow;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 테스트를 진행할때 JUnit에 내장된 실행자가 아닌 스프링실행자인 SpringRunner를 사용한다
@@ -27,5 +30,16 @@ public class HelloControllerTest {
             .andExpect(content().string(hello)); // mvc.poerform의 결과를 검증, 응답 본문의 내용을 검증한다. Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증한다.
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
 
+        mvc.perform(get("/hello/dto")
+                .param("name", name) // param : API 테스트 할때 사용될 요청 파라미터 설정, 단 값은 String만 허용. 숫자/날짜데이터는 등록할땐 문자열로 변경해야만 가능
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) // JSON 응답값을 필드별로 검증할 수 있는 메서드입니다. $를 기준으로 필드명을 명시. ex) $.name $.amount
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
 }
